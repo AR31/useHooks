019e15cea76b41b25547366aff3264fa6aec6266
arwa <arwa.khlifi@sofrecom>
2020-12-24 13:19:38 +0100

    update css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import MovieList from "./components/MovieList/MovieList";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
+    <>
+    <div className="row"><p className="titleMovie">List Movie</p></div>
     <div className="MovieList">
       <MovieList></MovieList>
     </div>
+    </>
   );
 }
 
